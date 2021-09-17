fe48c9eace76dfae5867c2a3c879063e97b42f4f
shaukat-ishtiaq <76278543+shaukat-ishtiaq@users.noreply.github.com>
2021-09-17 12:10:08 +0530

    Add files via upload

---
+++ mergeSortVsBubbleSort.c
@@ -0,0 +1,123 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+void swap(int *xp, int *yp)
+{
+    int temp = *xp;
+    *xp = *yp;
+    *yp = temp;
+}
+
+// A function to implement bubble sort
+void bubbleSort(int arr[], int n)
+{
+   int i, j;
+   for (i = 0; i < n-1; i++)
+
+       // Last i elements are already in place
+       for (j = 0; j < n-i-1; j++)
+           if (arr[j] > arr[j+1])
+              swap(&arr[j], &arr[j+1]);
+}
+
+/* Function to print an array */
+void printArray(int arr[], int size)
+{
+    int i;
+    for (i=0; i < size; i++)
+        printf("%d ", arr[i]);
+    printf("\n");
+}
+
+void merge(int arr[], int l, int m, int r)
+{
+    int i, j, k;
+    int n1 = m - l + 1;
+    int n2 = r - m;
+
+    /* create temp arrays */
+    int L[n1], R[n2];
+
+    /* Copy data to temp arrays L[] and R[] */
+    for (i = 0; i < n1; i++)
+        L[i] = arr[l + i];
+    for (j = 0; j < n2; j++)
+        R[j] = arr[m + 1 + j];
+
+    /* Merge the temp arrays back into arr[l..r]*/
+    i = 0; // Initial index of first subarray
+    j = 0; // Initial index of second subarray
+    k = l; // Initial index of merged subarray
+    while (i < n1 && j < n2) {
+        if (L[i] <= R[j]) {
+            arr[k] = L[i];
+            i++;
+        }
+        else {
+            arr[k] = R[j];
+            j++;
+        }
+        k++;
+    }
+
+    /* Copy the remaining elements of L[], if there
+    are any */
+    while (i < n1) {
+        arr[k] = L[i];
+        i++;
+        k++;
+    }
+
+    /* Copy the remaining elements of R[], if there
+    are any */
+    while (j < n2) {
+        arr[k] = R[j];
+        j++;
+        k++;
+    }
+}
+
+/* l is for left index and r is right index of the
+sub-array of arr to be sorted */
+void mergeSort(int arr[], int l, int r)
+{
+    if (l < r) {
+        // Same as (l+r)/2, but avoids overflow for
+        // large l and h
+        int m = l + (r - l) / 2;
+
+        // Sort first and second halves
+        mergeSort(arr, l, m);
+        mergeSort(arr, m + 1, r);
+
+        merge(arr, l, m, r);
+    }
+}
+
+int main() {
+    int arr[] = {1,3,42,2,41,66,66,33,98,0};
+
+     int n = sizeof(arr)/sizeof(arr[0]);
+     int arr_size = sizeof(arr) / sizeof(arr[0]);
+
+     time_t start1, end1, start2, end2;
+
+     start1 = time(NULL);
+     bubbleSort(arr, n);
+     end1 = time(NULL);
+
+     printf("Sorted array using bubble sort: \n");
+     printArray(arr, n);
+
+     start2 = time(NULL);
+     mergeSort(arr, 0, arr_size - 1);
+     end2 = time(NULL);
+
+     printf("\nSorted array using merge sort: \n");
+     printArray(arr, arr_size);
+
+     printf("\nTime taken for bubble sorting: %.2f seconds", difftime(end1, start1));
+     printf("\nTime taken for merge sorting: %.2f seconds", difftime(end2, start2));
+     return 0;
+}
